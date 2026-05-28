00dbe7032e9c8990544bd4f47a882e2597ff2eb2
25p61a05e3-collab <25p61a05e3@vbithyd.ac.in>
2026-05-28 16:25:04 +0530

    Add files via upload

---
+++ middleware/auth.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+
+const auth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  console.log("AUTH HEADER:", authHeader); // DEBUG
+
+  if (!authHeader) {
+    return res.status(401).json({ message: "No token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET || "secretkey"
+    );
+
+    req.user = decoded;
+    next();
+  } catch (err) {
+    return res.status(401).json({ message: "Invalid token" });
+  }
+};
+
+module.exports = auth;
